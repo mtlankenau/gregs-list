dfca5d3df0aa847d5f50648c251ea8d676c43ecb
jyanev01 <jordanyanev@gmail.com>
2022-01-29 18:47:18 -0500

    attempt at css

--- client/src/components/navBar.js
+++ client/src/components/navBar.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Link } from 'react-router-dom';
 import { Menu, Icon } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
 
 
 const NavBar = () => {
