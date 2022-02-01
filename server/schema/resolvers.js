a15f140176e25458170b480f0ed4d18a1eb4e1f5
mtlankenau <mtlankenau@gmail.com>
2022-02-01 15:10:39 -0500

    incorporated signToken function with addUser mutation. also updated params for payload when signing token to include other required signup fields, including firstName, lastName, & phoneNumber

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -40,6 +40,7 @@ const resolvers = {
   Mutation: {
     addUser: async(parents, args) => {
       const user = await User.create(args);
+      const token = signToken(user);
       return user;
     },
 
