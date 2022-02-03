ea27375cd078e41d85d8fb7fdc6e8ae6960a631d
DanWilk <dcwilkinson@wilktech.com>
2022-02-02 21:57:04 -0500

    added query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -11,7 +11,7 @@ export const ME = gql`
     }
 `;
 
-export const GET_POSTS = gql `
+export const GET_POSTS = gql`
     query posts {
         _id
         postType
@@ -26,6 +26,64 @@ export const GET_POSTS = gql `
             responseText
             username
             createdAt
+        }
+    }
+`
+
+export const GET_SINGLE_POST = gql`
+    post(_id: $_id) {
+        _id
+        postType
+        postCategory
+        postTitle
+        postPrice
+        postDescription
+        createdAt
+        username
+    }
+`
+
+export const GET_USER = gql`
+    users {
+        _id
+        username
+        email
+        firstName
+        lastName
+        phoneNumber
+        posts {
+            _id
+            postType
+            postCategory
+            postTitle
+            postPrice
+            postDescription
+            createdAt
+            username
+            responses {
+                _id
+                responseText
+                username
+                createdAt
+            }
+            responseCount
+        }
+        postCount
+    }
+`
+
+export const GET_SINGLE_USER = gql`
+    user(username: $username) {
+        _id
+        username
+        posts {
+            _id
+            createdAt
+            postType
+            postCategory
+            postTitle
+            postPrice
+            postDescription
         }
     }
 `
