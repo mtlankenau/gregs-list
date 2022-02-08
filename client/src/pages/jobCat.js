120042ca2590257eaefc4e84c1f7686dee729b18
jyanev01 <jordanyanev@gmail.com>
2022-02-08 18:52:12 -0500

    styling jobCat

--- client/src/pages/jobCat.js
+++ client/src/pages/jobCat.js
@@ -20,7 +20,7 @@ export default function JobCat() {
         return <div><p>#x1F354</p> Loading...</div>
     }
         return(
-            <Container minW='100%' minH='100%'>
+            <Container minW='100%' minH='100%' bg='green.500'>
                <Box>
                    {/* {posts.postByCat.postTitle} Category: {postCategory} */}
                </Box>
@@ -55,45 +55,17 @@ export default function JobCat() {
                             </Text>
                             
                         </Box>
-                        <Stack my={4} direction={'row'} spacing={100} align={'center'}>
-                            <Stack mt={6} direction={'column'} spacing={0} fontSize={'sm'}>
+                        <Stack my={2} direction={'row'} spacing={100} align={'center'}>
+                            <Stack  direction={'column'} spacing={0} fontSize={'sm'} >
                                 <Text fontWeight={600}>${post.postPrice}</Text>
                             </Stack>
-                            <Stack direction={'column'} spacing={0} fontSize={'sm'} >
-                                <Text fontWeight={600}>{post.username}</Text>
-                                <Text color={'gray.500'}>{post.createdAt}</Text>
+                            <Stack direction={'row '} spacing={0} fontSize={'sm'} >
+                                <Text fontWeight={600}>Posted by: {post.username}</Text>
+                                <Text color={'gray.500'}> @ {post.createdAt}</Text>
                                 </Stack>
                         </Stack>
                     </Box>
                    ))}
-                    {/* <Box
-                        w='100%'
-                        rounded='5px'
-                        overflow='hidden'
-                        boxShadow='sm'
-                        bg='gray.200'>
-                        
-                        <Box p={4}>
-                            <Stack align='baseline'>
-                                <Text as='h2' fontWeight='semibold' fontSize='xl' my={2}>Music Lessons</Text>
-                            </Stack>
-                            <Text fontWeight='light' fontSize='md'>
-                                Local music lessons from the people within your community. Video lessons are also available!
-                            </Text>
-                            
-                        </Box>
-                        <Stack my={4} direction={'row'} spacing={100} align={'center'}>
-                            <Stack direction={'column'} spacing={0} fontSize={'sm'}>
-                                <Text fontWeight={600}>Username</Text>
-                                <Text fontWeight={600}>First Last</Text>
-                                <Text color={'gray.500'}>Feb 05, 2022</Text>
-                            </Stack>
-                            <Stack direction={'column'} spacing={0} fontSize={'sm'}>
-                                <Text fontWeight={600}>Email</Text> 
-                                <Text color={'gray.500'}>555-555-5555</Text>
-                            </Stack>
-                        </Stack> 
-                    </Box> */}
                 </Wrap>
            </Container>
         );
